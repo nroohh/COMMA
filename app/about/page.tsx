4bc2154c9450d1e477fd6848ef808bf27c124eb5
Yewon Lim <yw09033@gmail.com>
2025-07-28 16:09:55 +0900

    just test

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-        about
+        about <br />gm
+        why can't I change the color of this 
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Link from 'next/link';
+import "./globals.css";
 
 export default function Home() {
   return (
-    <div>
-      HELLO
-    </div>
+    <>
+      <div>Wow</div>
+      <Link href="/about">Go to About Page</Link>
+    </>
   );
 }
