c8dd2aa00046d3f37842414f670e6c1b8c8da9a4
nroohh <nroohh@gmail.com>
2025-08-25 02:27:05 +0900

    commit11

--- app/api/admin/experience/[id]/route.ts
+++ app/api/admin/experience/[id]/route.ts
@@ -21,9 +21,9 @@ export async function DELETE(request: Request, context: { params: { id: string }
 
 export async function GET(
   request: NextRequest,
-  context: { params: { id: string } } // This is the correct signature
+  context: { params: Promise<{ id: string }> } // This is the correct signature
 ) {
-  const { id } = context.params; // Destructure `id` from `context.params`
+  const { id } = await context.params; // Destructure `id` from `context.params`
 
   try {
     await dbConnect();
