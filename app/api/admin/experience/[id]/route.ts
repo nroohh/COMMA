a1c675a93da1abe6c9f5c3e1d13752e1bda94493
nroohh <nroohh@gmail.com>
2025-08-25 01:55:48 +0900

    commit4

--- app/api/admin/experience/[id]/route.ts
+++ app/api/admin/experience/[id]/route.ts
@@ -2,19 +2,17 @@ import { NextResponse } from "next/server";
 import { dbConnect } from "@/app/_lib/db";
 import { Experience } from "@/app/_models/experience";
 
-// DATA STRUCTURE
-
 export async function DELETE(request: Request, context: { params: { id: string } }) {
     const { id } = context.params;
     try {
         await dbConnect();
         const deletedExperience = await Experience.findByIdAndDelete(id);
         if (!deletedExperience) {
-            return NextResponse.json({ error: "not found" }, { status: 404 }); // not found status
+            return NextResponse.json({ error: "not found" }, { status: 404 });
         }
-        return NextResponse.json({ message: "deleted successfully" }, { status: 200 }); // ok status
-    } catch (error: any) {
-        return NextResponse.json({ error: "internal server error" }, { status: 500 }); // internal server error status
+        return NextResponse.json({ message: "deleted successfully" }, { status: 200 });
+    } catch (error: unknown) {
+        return NextResponse.json({ error: "internal server error" }, { status: 500 });
     }
 }
 
@@ -24,11 +22,11 @@ export async function GET(request: Request, context: { params: { id: string } })
         await dbConnect();
         const experience = await Experience.findById(id);
         if (!experience) {
-            return NextResponse.json({ error: "not found" }, { status: 404 }); // not found status
+            return NextResponse.json({ error: "not found" }, { status: 404 });
         }
-        return NextResponse.json(experience, { status: 200 }); // ok status
-    } catch (error: any) {
-        return NextResponse.json({ error: "internal server error" }, { status: 500 }); // internal server error status
+        return NextResponse.json(experience, { status: 200 });
+    } catch (error: unknown) {
+        return NextResponse.json({ error: "internal server error" }, { status: 500 });
     }
 }
 
@@ -37,15 +35,17 @@ export async function PUT(request: Request, context: { params: { id: string } })
     try {
         const experience: ExperienceType = await request.json();
         if (!experience.title || !experience.cover || !experience.evidence || !experience.strands || !experience.timeline || !experience.dates || !experience.members || !experience.los || !experience.description || !experience.tips) {
-            return NextResponse.json({ error: "bad request" }, { status: 400 }); // bad request status
+            return NextResponse.json({ error: "bad request" }, { status: 400 });
         }
+
         await dbConnect();
         const updatedExperience = await Experience.findByIdAndUpdate(id, experience, { new: true });
         if (!updatedExperience) {
-            return NextResponse.json({ error: "not found" }, { status: 404 }); // not found status
+            return NextResponse.json({ error: "not found" }, { status: 404 });
         }
-        return NextResponse.json(updatedExperience, { status: 200 }); // ok status
-    } catch (error) {
-        return NextResponse.json({ error: "internal server error" }, { status: 500 }); // internal server error status
+
+        return NextResponse.json(updatedExperience, { status: 200 });
+    } catch (error: unknown) {
+        return NextResponse.json({ error: "internal server error" }, { status: 500 });
     }
 }
